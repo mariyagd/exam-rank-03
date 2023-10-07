2baaba229e48102e395093d247dbc75b5db656d5
mariyagd <109855801+mariyagd@users.noreply.github.com>
2023-10-07 22:57:25 +0200

    ok

---
+++ test.c
@@ -0,0 +1,13 @@
+/*
+ Created by Mariya Dancheva on 07.10.23.
+*/
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+    printf("max uint = %u\n", UINT_MAX);
+   // printf("min uint = %u\n", UINT_MIN);
+    return (0);
+}
